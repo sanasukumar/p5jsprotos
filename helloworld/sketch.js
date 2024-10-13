9159f8ab3385aaf0f1cde22c1c9854c839952151
sanasukumar <sana.kooka@gmail.com>
2024-10-13 17:20:14 +0530

    Add files via upload

---
+++ helloworld/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 200); // Set up the canvas
+  textAlign(CENTER, CENTER); // Align text to the center
+  textSize(32); // Set text size
+}
+
+function draw() {
+  background(220); // Light gray background
+  fill(0); // Set text color to black
+  text("Hello, World!", width / 2, height / 2); // Display text in the center
+}
