e4f8a83104a6b31c3937fc86974a9daf032a5ab0
sanasukumar <sana.kooka@gmail.com>
2024-10-13 17:31:58 +0530

    Add files via upload

--- helloworld/sketch.js
+++ helloworld/sketch.js
@@ -7,5 +7,5 @@ function setup() {
 function draw() {
   background(220); // Light gray background
   fill(0); // Set text color to black
-  text("Hello, World!", width / 2, height / 2); // Display text in the center
+  text("Hello, World from Sana Sukumar!", width / 2, height / 2); // Display text in the center
 }
